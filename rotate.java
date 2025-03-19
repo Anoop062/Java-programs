16f2c80a35318796dcf88a3ae6b61b2f3005e06c
Anoop062 <720722104062@hicet.ac.in>
2025-03-19 10:27:27 +0530

    Add files via upload

---
+++ rotate.java
@@ -0,0 +1,72 @@
+public class rotate {
+    public static void Clockwise(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void AntiClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = matrix[top][j];
+                matrix[top][j] = matrix[bottom][j];
+                matrix[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+    public static void callMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 0, 1, 2},
+            {3, 4, 5, 6}
+        };
+
+        System.out.println("Original Matrix:");
+        callMatrix(matrix);
+        Clockwise(matrix);
+        System.out.println("\n90 degrees clockwise rotation:");
+        callMatrix(matrix);
+        AntiClockwise(matrix);
+        System.out.println("\n90 degrees Anticlockwise rotation:");
+        callMatrix(matrix);
+    }
+} 
+    
+
